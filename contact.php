89925583fa19ca09346c680d8b15143de78b07f7
SuyesS <1by17is070@bmsit.in>
2021-01-13 22:27:38 +0530

    Ecommerce

---
+++ contact.php
@@ -0,0 +1,64 @@
+ <div id="contact-page" class="container">
+        <div class="bg">
+            <div class="row">           
+                <div class="col-sm-12">                         
+                    <h2 class="title text-center">Contact <strong>Us</strong></h2>                                
+                    <center><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3885.4357629986866!2d77.5643231143258!3d13.134890414829208!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x49c4c47a68a60b9c!2sBMS%20Institute%20of%20Technology%20and%20Management!5e0!3m2!1sen!2sin!4v1610276013912!5m2!1sen!2sin" width="800" height="450" frameborder="1" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></center>
+                </div>                  
+            </div>      
+            <div class="row">   
+                <div class="col-sm-8">
+                    <div class="contact-form">
+                        <h2 class="title text-center">Get In Touch</h2>
+                        <div class="status alert alert-success" style="display: none"></div>
+                        <form id="main-contact-form" class="contact-form row" name="contact-form" method="post">
+                            <div class="form-group col-md-6">
+                                <input type="text" name="name" class="form-control" required="required" placeholder="Name">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <input type="email" name="email" class="form-control" required="required" placeholder="Email">
+                            </div>
+                            <div class="form-group col-md-12">
+                                <input type="text" name="subject" class="form-control" required="required" placeholder="Subject">
+                            </div>
+                            <div class="form-group col-md-12">
+                                <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Your Message Here"></textarea>
+                            </div>                        
+                            <div class="form-group col-md-12">
+                                <input type="submit" name="submit" class="btn btn-primary pull-right" value="Submit">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="contact-info">
+                        <h2 class="title text-center">Contact Info</h2>
+                        <address>
+                            <p>E-Shopper Inc.</p>
+                            <p>Doddaballapur Main Road, Avalahalli</p>
+                            <p>Bengaluru-560064, India</p>
+                            <p>Fax: 1-999-999-9999</p>
+                            <p>Email: info@e-shopper.com</p>
+                        </address>
+                        <div class="social-networks">
+                            <h2 class="title text-center">Social Networking</h2>
+                            <ul>
+                                <li>
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-youtube"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>              
+            </div>  
+        </div>  
+    </div>
